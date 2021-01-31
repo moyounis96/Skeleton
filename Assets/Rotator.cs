d8f0a5edba148202d5c454df14e6bae3b24c2e31
Ahmed Qaddora <hem-plo@hotmail.com>
2021-01-31 09:33:18 +0200

    upload project

---
+++ Assets/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 300;
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * speed, 0);       
+    }
+}
---
+++ Assets/Scripts/SpawnSkeleton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class SpawnSkeleton : MonoBehaviour
+{
+    private ARRaycastManager _raycastManager;
+    private GameObject defaultSkeleton;
+    public GameObject skeletonPrefab;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private Transform spawnedSkeleton;
+    void Awake()
+    {
+        _raycastManager = GetComponent<ARRaycastManager>();
+        Application.targetFrameRate = 500;
+    }
+    void Start()
+    {
+        defaultSkeleton = Camera.main.transform.GetChild(0).gameObject;
+    }
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            if(_raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+            {
+                var hitPose = hits[0].pose;
+                if (!spawnedSkeleton)
+                {
+                    spawnedSkeleton = Instantiate(skeletonPrefab, hitPose.position, hitPose.rotation).transform;
+                    spawnedSkeleton.localScale = Vector3.one * 0.04f;
+                }
+                else
+                {
+                    spawnedSkeleton.position = hitPose.position;
+                    spawnedSkeleton.rotation = hitPose.rotation;
+                }
+                defaultSkeleton.SetActive(false);
+            }
+            else
+            {
+                defaultSkeleton.SetActive(!spawnedSkeleton);
+            }
+        }
+    }
+}
