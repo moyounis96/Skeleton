6af9aff563fde5b6fb4988fc3c9702745ae315c0
Ahmed Qaddora <hem-plo@hotmail.com>
2021-03-30 00:28:49 +0300

    Finish Project

--- Assets/Scripts/ModelPiece.cs
+++ Assets/Scripts/ModelPiece.cs
@@ -14,34 +14,11 @@ public class ModelPiece: MonoBehaviour
         if (string.IsNullOrEmpty(title))
             title = gameObject.name.Substring(gameObject.name.IndexOf("_")).Replace("_", " ");
         if (string.IsNullOrEmpty(description))
-            StartCoroutine(GetDescription());
+            description = "Look " + title + " on wikipedia";
     }
     
     void OnMouseDown()
     {
         SpawnSkeleton.Instance.ShowInformation(title, description);
-    }
-
-    IEnumerator GetDescription()
-    {
-        LoadingScreen.Instance.Show("Getting model's data...");
-        using (UnityWebRequest webRequest = UnityWebRequest.Get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + title))
-        {
-            yield return webRequest.SendWebRequest();
-            LoadingScreen.Instance.Hide();
-            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
-            {
-                description = "Look " + title + " on wikipedia";
-            }
-            else
-            {
-                string result = webRequest.downloadHandler.text;
-                if (result.Contains("\"extract\":\""))
-                    description = result.Substring(result.IndexOf("\"extract\":\"")).Replace("\"}}}}", "").Replace("\"extract\":\"", "").Replace("\\n","").Replace("\\r", "").Replace("\\","");
-                else
-                    description = "Look " + title + " on google";
-            }
-            Debug.Log(description);
-        }
     }
 }
